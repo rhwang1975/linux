cf4f7c7358c8cd38d863a3750d4d4e4b18ad759a
linux <39551578+rhwang1975@users.noreply.github.com>
2018-05-23 17:45:57 +0800

    Update rocketmq_install.sh

--- rocketmq/rocketmq_install.sh
+++ rocketmq/rocketmq_install.sh
@@ -245,6 +245,9 @@ echo "accessMessageInMemoryMaxRatio=40" >> ./broker-b.properties
 echo "messageDelyLevel=1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h" >> ./broker-b.properties
 ==============================================================================
 
+启动mqbroker
+nohup sh mqbroker -c /usr/local/rocketmq/conf/2m-noslave/broker-a.properties > /usr/local/rocketmq/conf/2m-noslave/broker-a.properties.out &
+nohup sh mqbroker -c /usr/local/rocketmq/conf/2m-noslave/broker-b.properties > /usr/local/rocketmq/conf/2m-noslave/broker-b.properties.out &
 
 
 
