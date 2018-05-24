ee59e0a14c70fe88a14907b5b32b46d513c01f9f
linux <39551578+rhwang1975@users.noreply.github.com>
2018-05-24 19:21:08 +0800

    Update rocketmq-externals-master-install.sh

--- rocketmq/rocketmq-externals-master-install.sh
+++ rocketmq/rocketmq-externals-master-install.sh
@@ -2,4 +2,9 @@ cd /soft/
 unzip rocketmq-externals-master.zip -d /usr/local/
 cd /usr/local/rocketmq-externals-master/
 find ./ -name application.properties
-cd rocketmq-console/src/main/resources 
+cd /usr/local/rocketmq-externals-master/rocketmq-console/src/main/resources 
+sed -ri '/^rocketmq.config.namesrvAddr=/c\rocketmq.config.namesrvAddr=192.168.150.145:9876;192.168.150.146:9876;192.168.150.147:9876' ./application.properties
+cd /usr/local/rocketmq-externals-master/rocketmq-console/src/test/resources
+sed -ri '/^rocketmq.config.namesrvAddr=/c\rocketmq.config.namesrvAddr=192.168.150.145:9876;192.168.150.146:9876;192.168.150.147:9876' ./application.properties
+cd /usr/local/rocketmq-externals-master/rocketmq-console/
+
