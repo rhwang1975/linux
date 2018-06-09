fce0deb57f7a1f3ccd454ab74b5c078cf0e529a7
linux <39551578+rhwang1975@users.noreply.github.com>
2018-06-09 16:20:24 +0800

    Update k8s_install.sh

--- k8s/k8s_install.sh
+++ k8s/k8s_install.sh
@@ -453,8 +453,11 @@ etcdctl --endpoints=https://192.168.150.181:2379,https://192.168.150.182:2379,ht
   --key-file=/etc/etcd/ssl/etcd-key.pem  cluster-health
 
 #五、所有节点安装配置docker
-
-
+#1. 安装docker（kubeadm目前支持docker最高版本是17.03.x）
+yum install https://mirrors.aliyun.com/docker-ce/linux/centos/7/x86_64/stable/Packages/docker-ce-selinux-17.03.2.ce-1.el7.centos.noarch.rpm  -y
+yum install https://mirrors.aliyun.com/docker-ce/linux/centos/7/x86_64/stable/Packages/docker-ce-17.03.2.ce-1.el7.centos.x86_64.rpm  -y
+#2. 修改配置文件 vim /usr/lib/systemd/system/docker.service
+ExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock  --registry-mirror=https://ms3cfraz.mirror.aliyuncs.com
 
 
 
